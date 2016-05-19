a1c942752270e67d4334a88f49dd6a8c1c705596
shouston3 <shouston3@me.com>
2016-05-19 08:09:18 +0100

    first commit

---
+++ server2.js
@@ -0,0 +1,30 @@
+const Hapi = require('hapi');
+const port = process.env.PORT || 4000
+
+const server = new Hapi.Server();
+server.connection({ port: port });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, server2!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/sayhello2',
+    handler: function (request, reply) {
+      console.log('working2');
+      reply('Howdy, server123456!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
